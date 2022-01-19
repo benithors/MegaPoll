fbe440bd07d9aa244bf7b25ea7c4e715e4353f89
Beni <thorstensen.benjamin@gmail.com>
2022-01-20 00:34:31 +0100

    getserversideprops types

--- pages/poll/[id].tsx
+++ pages/poll/[id].tsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import {supabase} from "../../utils/SupabaseClient";
 import {definitions} from "../../types/database";
+import {GetServerSideProps,NextPageContext } from "next";
 
+
+interface Context extends NextPageContext {
+    // any modifications to the default context, e.g. query types
+}
 
 interface IProps {
     allPolls:definitions["polls"],
-    something:string
 }
 
-export async function getServerSideProps ({ params }) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
     const allPolls = await supabase
         .from<definitions["polls"]>("polls")
-        .select("*").eq("uuid", params.id);
+        .select("*")
+        .eq("uuid",context.params?.id as string );
+
+
+
+
+
+
+
 
 
 
     return{
         props:{
             allPolls:allPolls,
-            something:params.id
         },
     };
 
@@ -30,6 +41,7 @@ const poll = (props: IProps) => {
     return (
         <div>
             {props.allPolls.poll_name}
+            {JSON.stringify(props.allPolls,null,2)}
         </div>
 
 
