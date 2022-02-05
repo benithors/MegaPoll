5c9a862879fa6ae29bbc49e0855c16bd39d12009
Beni <thorstensen.benjamin@gmail.com>
2022-02-05 14:51:07 +0100

    refactored to have connection between votes and users/cookies

--- pages/poll/[id].tsx
+++ pages/poll/[id].tsx
@@ -2,73 +2,82 @@ import React, {useEffect, useState} from 'react'
 import {supabase} from "../../utils/SupabaseClient";
 import {definitions} from "../../types/database";
 import {GetServerSideProps} from "next";
-import {checkCookies, getCookies, setCookies} from 'cookies-next';
-import { v4 as uuidv4 } from 'uuid';
+import {checkCookies, getCookie, setCookies} from 'cookies-next';
+import {v4 as uuidv4} from 'uuid';
 
 
 interface IProps {
     pollData: definitions["polls"],
-    pollQuestionsWrapper: IPollOption[],
+    pollQuestionsWrapper: IPollQuestion[],
 }
 
-interface IPollQuestion{
+interface IPollQuestion {
     pollQuestions: definitions["poll_questions"];
-    pollOptionsWrapper: IPollOption[];
-    voted:boolean;
-}
-
-interface IPollOption {
-    pollOptions: definitions["poll_options"]
+    pollOptions: definitions["poll_options"][]
+    voted: boolean;
 }
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
     const id = context.params.id;
-    const { req, res } = context;
+    const {req, res} = context;
     const pollData = await supabase
         .from<definitions["polls"]>("polls")
         .select("*")
         .eq("id", id.toString()).single();
-
+    //first we get the question
     const allQuestions = await supabase
         .from<definitions["poll_questions"]>("poll_questions")
         .select("*")
         .eq("poll", id.toString());
 
-    //first we get the question
+
+    let questionWrapper: IPollQuestion[] = [];
+
     //then we check if the user is logged in
     //if not we check if there is a cookie
-    //if there is one we have to check in the db if there is an entry in profiles_2_poll_options
-    //for a profile option which is connected to the question
-
-
-
-    const allPollOptions = await supabase
-        .from<definitions["poll_options"]>("poll_options")
-        .select("*")
-        .in("poll_question", allQuestions.data.map(a => a.id));
-
-
-    let pollOptionDataWrapper: IPollOption[] = [];
-
-    allPollOptions.data.forEach((value) => {
-        const temp: IPollOption = {
-            pollOptions: value,
-        }
-        pollOptionDataWrapper.push(temp);
-    })
-    if(!checkCookies('voter', context)){
-        setCookies('voter', uuidv4(), { req, res, maxAge: 604800});//a week
+    if (!checkCookies('voter', context)) {
+        setCookies('voter', uuidv4(), {req, res, maxAge: 604800});//a week
     }
+        //if there is one we have to check in the db if there is an entry in profiles_2_poll_options
+    //for a profile option which is connected to the question
+    else {
 
+
+        for (const pollQuestion of allQuestions.data) {
+
+            const allPollOptions = await supabase
+                .from<definitions["poll_options"]>("poll_options")
+                .select("*")
+                .eq("poll_question", pollQuestion.id);
+
+            const pollOptionsVoted = await supabase
+                .from<definitions["poll_options_voted"]>("poll_options_voted")
+                .select("*")
+                .eq("poll_question", pollQuestion.id)
+                .eq("cookie_identifier", getCookie('voter', context));
+
+            let didVote = pollOptionsVoted.data.some(value => value.voted);
+
+            const pollQuestionTemp: IPollQuestion = {
+                pollOptions: allPollOptions.data,
+                pollQuestions: pollQuestion,
+                voted: didVote
+            }
+            questionWrapper.push(pollQuestionTemp)
+
+
+        }
+
+
+    }
 
 
     return {
         props: {
             pollData: pollData.data,
-            pollQuestions: allQuestions.data,
-            pollOptionsWrapper: pollOptionDataWrapper
+            pollQuestionsWrapper: questionWrapper
         },
     }
 
@@ -78,36 +87,50 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const Poll = (props: IProps) => {
 
-    const [optionsData, setOptionsData] = useState<IPollOption[]>(props.pollOptionsWrapper)
+    const [optionsData, setOptionsData] = useState<IPollQuestion[]>(props.pollQuestionsWrapper)
     let mySubscription = [null];
 
 
-    const handleNewOptionsUpdate = (payload: { commit_timestamp?: string; eventType?: "INSERT" | "UPDATE" | "DELETE"; schema?: string; table?: string; new: any; old?: any; errors?: string[]; }) => {
+    const handleNewOptionsUpdate = (payload: { commit_timestamp?: string; eventType?: "INSERT" | "UPDATE" | "DELETE"; schema?: string; table?: string; new: definitions["poll_options"]; old?: any; errors?: string[]; }) => {
 
-        setOptionsData(prevState => {
-            let iPollOptions = prevState.slice();
-            let idx = iPollOptions.findIndex(x => x.pollOptions.id == payload.new.id);
-            const temp: IPollOption = {
-                pollOptions: payload.new,
-            }
-            iPollOptions[idx] = temp;
-            return [...iPollOptions]
-        });
+           setOptionsData(prevState => {
+               let prevStatePollQuestionWrapper = prevState.slice();
+               let questionIdx = prevStatePollQuestionWrapper.findIndex(value => value.pollQuestions.id === payload.new.poll_question);
+/*
+               let iPollQuestion = prevStatePollQuestionWrapper.find(value => value.pollQuestions.id === payload.new.poll_question);
+*/
+
+               let iPollQuestion = prevStatePollQuestionWrapper[questionIdx];
+               let optionIdx = iPollQuestion.pollOptions.findIndex(x =>
+                   x.id == payload.new.id);
+
+
+               const tempOption: definitions["poll_options"] = {
+                   option:payload.new.option,
+                   id:payload.new.id,
+                   votes:payload.new.votes,
+                   poll_question:payload.new.poll_question
+               }
+               iPollQuestion.pollOptions[optionIdx] = tempOption;
+               prevStatePollQuestionWrapper[questionIdx] = iPollQuestion;
+               return [...prevStatePollQuestionWrapper]
+           });
 
     };
 
 
-
     useEffect(() => {
 
-        props.pollOptionsWrapper.forEach((value) => {
-            let subTemp = supabase
-                .from('poll_options:id=eq.' + value.pollOptions.id)
-                .on('*', payload => {
-                    handleNewOptionsUpdate(payload);
-                })
-                .subscribe();
-            mySubscription.push(subTemp);
+        props.pollQuestionsWrapper.forEach((question) => {
+            question.pollOptions.forEach(option => {
+                let subTemp = supabase
+                    .from('poll_options:id=eq.' + option.id)
+                    .on('*', payload => {
+                        handleNewOptionsUpdate(payload);
+                    })
+                    .subscribe();
+                mySubscription.push(subTemp);
+            })
         })
 
 
@@ -121,45 +144,39 @@ const Poll = (props: IProps) => {
     }, [])  //todo need to figure out why removing the deps array breaks the updates
 
 
-
-
     //TODO write a function that takes an polloption id as parameter and inserts into
     // profiles_2_votes
 
 
-    function getVotePercentage(value, filteredOptions: IPollOption[]): number {
-        if (value.pollOptions.votes === 0) {
+    function getVotePercentage(value: definitions["poll_options"], options: definitions["poll_options"][]): number {
+        if (value.votes === 0) {
             return 0
         }
-        let map = filteredOptions.map(value1 => value1.pollOptions);
-        return (100 * value.pollOptions.votes) / map.reduce((a, b) => +a + +b.votes, 0);
+        return (100 * value.votes) / options.reduce((a, b) => +a + +b.votes, 0);
     }
-
 
     return (
         <div className={"w-full pt-16 px-20"}>
 
 
-            {props.pollQuestions.map((pollque, index) => {
-                    const filteredOptions = optionsData.filter(value => value.pollOptions.poll_question === pollque.id);
+            {props.pollQuestionsWrapper.map((pollQ, index) => {
                     return <div key={index}>
-                        {pollque.question}
+                        {pollQ.pollQuestions.question}
 
 
                         <div className="p-6 space-y-2 artboard  w-full ">
-                            {filteredOptions.map((value, idx) =>
+                            {pollQ.pollOptions.map((value, idx) =>
                                 <div key={idx}>
                                     <div>
-                                        {value.pollOptions.option}
+                                        {value.option}
                                     </div>
 
                                     <div className={'flex flex-row justify-between'}>
                                         <div className={'w-2/4'}>
-
-                                            <progress className="progress progress-primary" value={getVotePercentage(value, filteredOptions)} max="100"></progress>
+                                            <progress className="progress progress-primary" value={getVotePercentage(value, pollQ.pollOptions)} max="100"/>
                                         </div>
                                         <div>
-                                            Votes {value.pollOptions.votes} | {getVotePercentage(value, filteredOptions).toFixed(1)}%
+                                            Votes {value.votes} | {getVotePercentage(value, pollQ.pollOptions).toFixed(1)}%
                                         </div>
                                     </div>
                                 </div>
