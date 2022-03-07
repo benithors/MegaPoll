855db9d7cbb054d4d30a215e677a3f57c6104465
Beni <thorstensen.benjamin@gmail.com>
2022-03-07 21:41:45 +0100

    unlimited creation of poll options

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,40 +1,45 @@
 import React from 'react'
 
 
-interface IPoll {
-    pollName: string
-    pollOptions: string[]
-}
 
 const Poll = () => {
-    const [poll, setPoll] = React.useState<IPoll>({
-        pollOptions: [''],
-        pollName: ''
-    });
+    const [poll, setPoll] = React.useState<string>();
+    const [pollOptions, setPollOptions] = React.useState<string[]>(['']);
 
+    function isNotEmpty(lastPollOption: string) {
+        return lastPollOption !== undefined && lastPollOption !== null && lastPollOption !== '';
+    }
 
-    function updatePoll(index: number, e: { target: { value: string; }; }) {
-        let newPollOpt = [...poll.pollOptions]
-        newPollOpt[index] = e.target.value;
-        let newPoll = poll;
-        poll.pollOptions = newPollOpt;
-        console.log(newPoll)
-        setPoll(newPoll)
+    function updatePollOptions(index: number, e: { target: { value: string; }; }) {
+        setPollOptions(prevState => {
+            const pollOptions = [...prevState];
+            let pollOption = pollOptions[index];
+            pollOption = e.target.value;
+            pollOptions[index] = pollOption;
+            //we need to check if the last element in poll has some string in it
+            //if this is so we need to append +1 on poll so that another poll can be added
+            const lastPollOption = pollOptions[pollOptions.length - 1];
+            if (isNotEmpty(lastPollOption)) {
+                pollOptions.push('')
+            }
+            return pollOptions;
+        });
     }
 
     return (
         <div className="card glass lg:card-side text-neutral-content">
             <div className="form-control">
-                <input value={poll.pollName}
-                       onChange={e => setPoll({...poll, pollName: e.target.value})}
+                <input value={poll}
+                       onChange={e => setPoll(e.target.value)}
                        type="text" placeholder="Type your question here" className="input input-primary input-bordered"/>
 
-                {poll.pollOptions?.map((value, index) => {
+                {pollOptions.map((value, index) => {
 
-                    return <input value={value}
-                                  key={index}
-                                  onChange={event => updatePoll(index, event)}
-                                  type="text" placeholder="Poll Option" className="input input-primary input-bordered"/>
+                    return <input
+                        key={index}
+                        onChange={event => updatePollOptions(index, event)}
+                        type="text" placeholder="Poll Option" className="input input-primary input-bordered
+                                  mt-2"/>
                 })}
 
             </div>
--- pages/create-poll.tsx
+++ pages/create-poll.tsx
@@ -1,42 +1,47 @@
 import React from 'react'
 import Poll from "../components/Poll";
+import {useForm} from "react-hook-form";
 
 
 const createPoll = () => {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const onSubmit = data => console.log(data);
 
     return (
+
         <main className={"flex flex-col items-center  h-screen"}>
-            <div className={"mt-16 text-4xl"}>
-                Create a Poll
-            </div>
-
-
-            <div className="form-control">
-                <label className="label">
-                    <span className="label-text">Poll Name</span>
-                </label>
-                <input type="text" placeholder="Give your Poll a name" className="input input-bordered"/>
-
-                <label className="label">
-                    <span className="label-text">Describe the poll</span>
-                </label>
-                <textarea className="textarea h-24 textarea-bordered" placeholder="Describe what the poll is about"></textarea>
-
-                <label className="label">
-                    <span className="label-text">Cover Image of the Poll</span>
-                </label>
-                <input type="file"/>
-
-                <div className={"mt-16"}>
-
-                    <Poll/>
-
-
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={"mt-16 text-4xl"}>
+                    Create a Poll
                 </div>
 
 
-            </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Poll Name</span>
+                    </label>
+                    <input type="text" placeholder="Give your Poll a name" className="input input-bordered" {...register("pollName")}/>
 
+                    <label className="label">
+                        <span className="label-text">Describe the poll</span>
+                    </label>
+                    <textarea className="textarea h-24 textarea-bordered" placeholder="Describe what the poll is about" {...register("pollDescription")}></textarea>
+
+                    <label className="label">
+                        <span className="label-text">Cover Image of the Poll</span>
+                    </label>
+                    <input type="file"/>
+
+                    <div className={"mt-16"}>
+
+                        <Poll/>
+
+
+                    </div>
+
+                    <input type="submit" />
+                </div>
+            </form>
 
         </main>
 
