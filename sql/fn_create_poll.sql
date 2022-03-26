917aced8120b3d60051f21b8e06ded79dc40379f
Beni <thorstensen.benjamin@gmail.com>
2022-03-26 23:39:51 +0100

    update database functions

--- sql/fn_create_poll.sql
+++ sql/fn_create_poll.sql
@@ -1,4 +1,4 @@
-create function fn_create_poll(poll_name character varying, poll_description character varying, poll_question_data json) returns uuid
+create function fn_create_poll(poll_name character varying, poll_description character varying, cover_image character varying, poll_question_data json) returns uuid
     language plpgsql
 as
 $$
@@ -11,11 +11,12 @@ DECLARE
     pollQuestion text;
     pollOptions json;
     multiPoll boolean;
+    poll_instance_uuid uuid;
 begin
 
     poll_uuid := gen_random_uuid();
-    INSERT INTO public.polls (poll_name, poll_description, uuid)
-    VALUES (poll_name, poll_description, poll_uuid)
+    INSERT INTO public.polls (poll_name, poll_description, uuid,cover_image)
+    VALUES (poll_name, poll_description, poll_uuid,cover_image)
     RETURNING id into pollId;
     --loop through all the question
     FOR questions IN
@@ -38,21 +39,22 @@ begin
             FOR  pollOption IN
                 SELECT * FROM json_array_elements(pollOptions)
                 LOOP
-                    RAISE NOTICE '%',trim(both '"' FROM pollOption);
+                    RAISE NOTICE '%',pollOption;
                     insert into poll_options(option, poll_question) VALUES (trim(both '"' FROM pollOption), questionId);
                 end loop;
 
         END LOOP;
 
-    return poll_uuid;
+
+    select fn_create_poll_from_template(poll_uuid) into poll_instance_uuid;
+    return poll_instance_uuid;
 end ;
 $$;
 
-alter function fn_create_poll(varchar, varchar, json) owner to postgres;
+alter function fn_create_poll(varchar, varchar, varchar, json) owner to postgres;
 
-grant execute on function fn_create_poll(varchar, varchar, json) to anon;
+grant execute on function fn_create_poll(varchar, varchar, varchar, json) to anon;
 
-grant execute on function fn_create_poll(varchar, varchar, json) to authenticated;
+grant execute on function fn_create_poll(varchar, varchar, varchar, json) to authenticated;
 
-grant execute on function fn_create_poll(varchar, varchar, json) to service_role;
-
+grant execute on function fn_create_poll(varchar, varchar, varchar, json) to service_role;
