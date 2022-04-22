bcc072eb9c3bb417e9d1fa42a9bdaa978f8add74
Benjamin Thorstensen <benjamin.thorstensen@compax.at>
2022-04-22 11:05:36 +0200

    styling

--- components/VoteBar.tsx
+++ components/VoteBar.tsx
@@ -132,19 +132,23 @@ const VoteBar = (props: IProps) => {
     }
     loadData();
   }, [props.pollOptionWrapper.pollOptionVotes.top_profile_3]);
+  let votePercentage = getVotePercentage(
+    props.pollOptionWrapper.pollOptionVotes.votes,
+    props.pollOptionWrapperArray
+  );
   return (
     <div>
-      <div>{props.pollOptionWrapper.pollOption.option}</div>
+      <div className={""}>{props.pollOptionWrapper.pollOption.option}</div>
       <div className={"flex flex-row justify-between"}>
-        <div className={"w-2/4"}>
-          <progress
-            className={"progress progress-primary"}
-            value={getVotePercentage(
-              props.pollOptionWrapper.pollOptionVotes.votes,
-              props.pollOptionWrapperArray
-            )}
-            max="100"
+        <div className={"flex w-2/4 flex-row items-center"}>
+          <div
+            className={
+              "h-4 bg-primary pr-1 opacity-90 drop-shadow-2xl transition-[width]"
+            }
+            style={{ width: `${votePercentage}%` }}
           />
+          <div className={"pl-1"}>{votePercentage.toFixed(1)}%</div>
+          {props.pollOptionWrapper.pollOptionVotes.id}
         </div>
         <div className="flex -space-x-3">
           {profile1 && <VoteBarProfile profile={profile1} />}
--- sql/useful.sql
+++ sql/useful.sql
@@ -8,18 +8,18 @@ for counter in 1..10000
                 PERFORM
                     pg_sleep(0.001);
                 raise notice 'counter: %', counter;
-update poll_options set votes = poll_options.votes + 1 where id = floor(random() * 3 + 1)::int;
+update poll_option_votes set votes = poll_option_votes.votes + 1 where id = floor(random() * (400-396 + 1)  + 396)::int;
 if (counter <100) then
-update poll_options set votes = poll_options.votes + 1 where id = 3;
+update poll_option_votes set votes = poll_option_votes.votes + 1 where id = 3;
 end if;
                 if (counter > 300) then
-update poll_options set votes = poll_options.votes + 1 where id = 2;
+update poll_option_votes set votes = poll_option_votes.votes + 1 where id = 2;
 end if;
-
-
 commit;
 end loop;
 
 end;
 $$
 ;
+
+update poll_option_votes set votes = 0 where id in (400,399,398,397,396);
