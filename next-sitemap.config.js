a09594571e55769c0b8d2ae1766036a0ff0dd5fa
Beni <thorstensen.benjamin@gmail.com>
2022-05-12 17:14:35 +0200

    dyanmic sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     siteUrl: 'https://socialpoll.me',
     generateRobotsTxt: true, // (optional)
+    changefreq: null,
+    priority: null,
     // ...other options
-    exclude: ['/contact','/privacy','/tos', '/imprint','/server-sitemap-index.xml'],
+    exclude: ['/contact','/privacy','/tos', '/about','/imprint','/server-sitemap-index.xml'],
     robotsTxtOptions: {
         additionalSitemaps: [
             'https://socialpoll.me/server-sitemap-index.xml', // <==== Add here
