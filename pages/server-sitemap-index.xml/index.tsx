0a13846899553731092b0180b0467682df2592d0
Beni <thorstensen.benjamin@gmail.com>
2022-05-12 17:06:06 +0200

    dyanmic sitemap config

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -1,23 +1,23 @@
 // pages/server-sitemap-index.xml/index.tsx
-import { getServerSideSitemapIndex } from 'next-sitemap'
-import { GetServerSideProps } from 'next'
-import {supabaseClient} from "@supabase/supabase-auth-helpers/nextjs";
-import {definitions} from "../../types/database";
+import { getServerSideSitemapIndex } from "next-sitemap";
+import { GetServerSideProps } from "next";
+import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import { definitions } from "../../types/database";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const addSiteMaps = []
-    const { data, error } = await supabaseClient
-        .from<definitions["front_page"]>("front_page")
-        .select("*");
+  const addSiteMaps = [];
+  const { data, error } = await supabaseClient
+    .from<definitions["front_page"]>("front_page")
+    .select("*");
 
-    if(data){
-        data.forEach(page => {
-            addSiteMaps.push({ loc: `https://socialpoll.me/`+ page.poll_instance })
-        })
-    }
+  if (data) {
+    data.forEach((page) => {
+      addSiteMaps.push('https://socialpoll.me/' + page.poll_instance);
+    });
+  }
 
-    return getServerSideSitemapIndex(ctx, addSiteMaps)
-}
+  return getServerSideSitemapIndex(ctx, addSiteMaps);
+};
 
 // Default export to prevent next.js errors
 export default function SitemapIndex() {}
