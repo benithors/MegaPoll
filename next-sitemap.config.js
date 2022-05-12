3a75eea9cf88e80edfaca18221feaaad97d47c20
Beni <thorstensen.benjamin@gmail.com>
2022-05-12 17:00:43 +0200

    dyanmic sitemap config and excluded some sites

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@ module.exports = {
     siteUrl: 'https://socialpoll.me',
     generateRobotsTxt: true, // (optional)
     // ...other options
+    exclude: ['/contact','/privacy','/tos', '/imprint','/server-sitemap-index.xml'],
+    robotsTxtOptions: {
+        additionalSitemaps: [
+            'https://socialpoll.me/server-sitemap-index.xml', // <==== Add here
+        ],
+    },
 }
