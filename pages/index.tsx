a07dc857ee013f087083c96c947e4fb4e554eca0
Beni <thorstensen.benjamin@gmail.com>
2022-05-31 15:15:19 +0200

    gradient styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,93 +50,101 @@ function Home(props: IProps) {
   }
 
   return (
-    <Container>
+      <>
 
-        <canvas
-            id="gradient-canvas"
-            className={
-                ''}
-        ></canvas>
-      <NextSeo
-        title="Socialpoll.me - Share your Polls"
-        description="Free realtime polls for you and your community"
-        robotsProps={{
-          nosnippet: true,
-          notranslate: false,
-          noimageindex: true,
-          noarchive: false,
-          maxSnippet: -1,
-          maxImagePreview: 'standard',
-          maxVideoPreview: -1
-        }}
-        twitter={{
-          handle: '@socialpollme',
-          site: '@socialpollme',
-          cardType: 'summary_large_image'
-        }}
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://www.socialpoll.me/',
-          site_name: 'SocialPoll.me',
-          images: [
-            {
-              url: 'https://pbs.twimg.com/profile_images/1522287055463714820/4TE0Pt3__400x400.jpg',
-              width: 400,
-              height: 400,
-              alt: 'socialpoll.me',
-              type: 'image/jpeg'
-            }
-          ]
-        }}
-      />
+          <div id="gradient-container">
 
-      <Title firstPart={'Share Your'} secondPart={'Opinion'} />
-      <div
-        className={
-          ' flex flex-col items-center self-center text-xl sm:text-2xl md:flex-row md:text-3xl z-10'
-        }
-      >
-        <div className={''}>
-          <span>Free</span>
+              <canvas
+                  id="gradient-canvas"
+                  className={
+                      ''}
+              ></canvas>
+          </div>
 
-          <span
-            className={
-              'mx-1 mx-1 rounded bg-gradient-to-l from-secondary to-primary px-1 '
-            }
-          >
+          <Container>
+
+              <NextSeo
+                  title="Socialpoll.me - Share your Polls"
+                  description="Free realtime polls for you and your community"
+                  robotsProps={{
+                      nosnippet: true,
+                      notranslate: false,
+                      noimageindex: true,
+                      noarchive: false,
+                      maxSnippet: -1,
+                      maxImagePreview: 'standard',
+                      maxVideoPreview: -1
+                  }}
+                  twitter={{
+                      handle: '@socialpollme',
+                      site: '@socialpollme',
+                      cardType: 'summary_large_image'
+                  }}
+                  openGraph={{
+                      type: 'website',
+                      locale: 'en_IE',
+                      url: 'https://www.socialpoll.me/',
+                      site_name: 'SocialPoll.me',
+                      images: [
+                          {
+                              url: 'https://pbs.twimg.com/profile_images/1522287055463714820/4TE0Pt3__400x400.jpg',
+                              width: 400,
+                              height: 400,
+                              alt: 'socialpoll.me',
+                              type: 'image/jpeg'
+                          }
+                      ]
+                  }}
+              />
+
+              <Title firstPart={'Share Your'} secondPart={'Opinion'} />
+              <div
+                  className={
+                      ' flex flex-col items-center self-center text-xl sm:text-2xl md:flex-row md:text-3xl z-10'
+                  }
+              >
+                  <div className={''}>
+                      <span>Free</span>
+
+                      <span
+                          className={
+                              'mx-1 mx-1 rounded bg-gradient-to-l from-secondary to-primary px-1 '
+                          }
+                      >
             realtime polls
           </span>
-        </div>
-        <div>
-          for you
-          <span className={'px-2 font-extrabold'}>and your community.</span>
-        </div>
-      </div>
+                  </div>
+                  <div>
+                      for you
+                      <span className={'px-2 font-extrabold'}>and your community.</span>
+                  </div>
+              </div>
 
 
-        <button className="btn btn-accent mt-4 mb-8 self-center text-2xl md:mt-14 z-10">
-            <Link href="/create-poll">CREATE A POLL</Link>
-        </button>
-        <PaddingContainer
-        className={
-          'grid w-full grid-cols-1 gap-4 self-center rounded-2xl  sm:grid-cols-2 md:w-11/12 md:pt-8 xl:grid-cols-3'
-        }
-      >
-        {props.frontPage.map((value: definitions['front_page'], index) => {
-          return (
-            <div key={index} className={'stream'}>
-              <PollPreviewCard
-                router={router}
-                key={index}
-                poll={value}
-                openInstance={openInstance}
-              />
-            </div>
-          );
-        })}
-      </PaddingContainer>
-    </Container>
+              <button className="btn btn-accent mt-4 mb-8 self-center text-2xl md:mt-14 z-10">
+                  <Link href="/create-poll">CREATE A POLL</Link>
+              </button>
+              <PaddingContainer
+                  className={
+                      'grid w-full grid-cols-1 gap-4 self-center rounded-2xl  sm:grid-cols-2 md:w-11/12 md:pt-8 xl:grid-cols-3'
+                  }
+              >
+                  {props.frontPage.map((value: definitions['front_page'], index) => {
+                      return (
+                          <div key={index} className={'stream'}>
+                              <PollPreviewCard
+                                  router={router}
+                                  key={index}
+                                  poll={value}
+                                  openInstance={openInstance}
+                              />
+                          </div>
+                      );
+                  })}
+              </PaddingContainer>
+          </Container>
+      </>
+
   );
 }
 
